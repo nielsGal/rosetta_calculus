3b5b331ce55b67ce2360379880283d48cab8f621
nielsGal <galjaardniels@gmail.com>
2019-11-19 00:26:26 +0100

    added sub in js

--- lambda_calc.js
+++ lambda_calc.js
@@ -1,3 +1,7 @@
+//use this function for testing, for example N5(P1)(0) should be 5
+const P1 = x => x + 1
+
+//combinators
 const I = a => a
 const M = a => a(a)
 const K = a => b => a
@@ -10,17 +14,7 @@ const KI = a => b => b
 const Y = f => M( x => f(M(x)))
 const Z = f => M(x => f( v=> M(x)(v)))
 
-const T = a => b => a
-const F = a => b => b
-const Not =  p => p(F)(T)
-const And = p => q => p(q)(p)
-const Or = p => q => p(p)(q)
-const Beq = p => q => p(q)((Not(q)))
-
-const succ = n => f => a => f(n(f)(a))
-const add = n => k => f => B(n(f))(k(f))
-const exp = n => k => k(n)
-
+//pair constructor and pair functions
 const pair = a => b => f => f(a)(b)
 const first = p => p(K)
 const second = p => p(KI)
@@ -28,13 +22,27 @@ const phi = p => pair(second(p))(succ(second(p)))
 const setFirst = c => p => pair(c)(second(p))
 const setSecond = c => p => pair(first(p))(c)
 
-const pred = n => first(n(phi)(pair(n0)(n0)))
-const sub = null
+//church booleans
+const T = a => b => a
+const F = a => b => b
 
-const n0 = f => a => a
-const n1 = f => a => f(a)
-const n2 = f => a => f(f(a))
-const n3 = f => a => f(f(f(a)))
-const n4 = f => a => f(f(f(f(a))))
-const n5 = f => a => f(f(f(f(f(a)))))
+//boolean functions, supply with T and F return T or F
+const Not =  p => p(F)(T)
+const And = p => q => p(q)(p)
+const Or = p => q => p(p)(q)
+const Beq = p => q => p(q)((Not(q)))
+
+//church number operators
+const succ = n => f => a => f(n(f)(a))
+const add = n => k => f => B(n(f))(k(f))
+const exp = n => k => k(n)
+const pred = n => first(n(phi)(pair(n0)(n0)))
+const sub = n => k => k(pred)(n)
+
+const N0 = f => a => a
+const N1 = f => a => f(a)
+const N2 = f => a => f(f(a))
+const N3 = f => a => f(f(f(a)))
+const N4 = f => a => f(f(f(f(a))))
+const N5 = f => a => f(f(f(f(f(a)))))
 
--- lambda_calc.py
+++ lambda_calc.py
@@ -1,6 +1,7 @@
+#use this to test, for example N5(P1)(0) should be 5
 P1 = lambda x: x + 1
 
-
+# some set of combinators
 I = lambda x: x
 M = lambda x: x(x)
 K = lambda a: lambda b: a
@@ -11,15 +12,17 @@ Th = lambda a: lambda f: f(a)
 V = lambda a: lambda b: lambda f: f(a)(b)
 B1 = lambda f: lambda g: lambda a: lambda b: f(g(a)(b))
 
+#booleans True and False
 T = lambda a: lambda b: a
 F = lambda a: lambda b: b
 
+#boolean functions, supply with T and F return T and F
 Not = lambda p: p(F)(T)
 And = lambda p: lambda q: p(q)(p)
 Or = lambda p: lambda q: p(p)(q)
 Beq = lambda p: lambda q: p(q)(Not(q))
 
-
+#some hard coded church numerals
 N0 = lambda f: lambda a: a 
 N1 = lambda f: lambda a: f(a)
 N2 = lambda f: lambda a: f(f(a))
@@ -34,6 +37,7 @@ phi = lambda p: pair(second(p))(succ(second(p)))
 setFirst = lambda c: lambda p: pair(c)(second(p))
 setSecond = lambda c: lambda p: pair(first(p))(c)
 
+#chuch bool operations
 succ = lambda n: lambda f: lambda a: f(n(f)(a))
 add = lambda n: lambda k: lambda f: B(n(f))(k(f))
 exp = lambda n: lambda k: k(n)
@@ -41,6 +45,7 @@ mult = lambda n: lambda k: lambda f: n(k(f))
 pred = lambda n: first( n (phi) (pair(N0)(N0)))
 sub = lambda n: lambda k: k(pred)(n)
 
+#these functions take Church numerals and return church bools
 isZero = lambda n: n(K(F))(T)
 LeQ = lambda n: lambda k: isZero(sub(n)(k))
 eQ = lambda n: lambda k: And(LeQ(n)(k))(LeQ(n)(k))
